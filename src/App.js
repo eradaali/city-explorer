fd4d0875097f930bf479b789709819ee9722ebb8
eradaali <erada.abdalrhman@gmail.com>
2021-06-29 15:29:39 +0300

    add

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
-import React, { Component } from 'react';
+
 import axios from 'axios';
-import Image from 'react-bootstrap/Image'
+import React, { Component } from 'react';
+import Image from 'react-bootstrap/Image';
+// import Alert from 'react-bootstrap/Alert';
+
 
 export class App extends Component {
 
@@ -20,33 +23,28 @@ export class App extends Component {
   }
   submitData=async (e)=>{
     e.preventDefault()
-    let axiosResponse = await axios.get(`https://eu1.locationiq.com/v1/search.php?key=pk.afaca60038b93085cf4229304fcb1ffc&city=${this.state.displayName}&format=json`)
-
-setTimeout(()=>{
-  this.setState({
-    displayName:axiosResponse.data[0].display_name,
-    longitude:axiosResponse.data[0].lon,
-    latitude:axiosResponse.data[0].lat
-  })
-},3000)
- 
+    let axiosResponse= await axios.get(`https://eu1.locationiq.com/v1/search.php?key=pk.60346fba30221450f0bd55e67928ff53&city=${this.state.displayName}&format=json`)
+    this.setState({
+      displayName:axiosResponse.data[0].display_name,
+      longitude:axiosResponse.data[0].lon,
+      latitude:axiosResponse.data[0].lat
+    })
     console.log(axiosResponse.data[0].lat)
   }
   render() {
     return (
       <div>
         <form onSubmit={this.submitData}>
-          <input type='text' placeholder='city name....' onClick={(e)=>{this.nameChangeHandler(e)}} />
+          <input type='text' placeholder='city name....' onChange={(e)=>{this.nameChangeHandler(e)}} />
           <button>Search</button>
         </form>
         <h1>{this.state.displayName}</h1>
         <h1>{this.state.longitude}</h1>
         <h1>{this.state.latitude}</h1>
         <Image src={`https://maps.locationiq.com/v3/staticmap?key=pk.afaca60038b93085cf4229304fcb1ffc&center=${this.state.latitude},${this.state.longitude}&zoom=20`} rounded/>
-        
       </div>
     )
   }
 }
 
-export default  App
+export default App
