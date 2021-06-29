a340153c03b328a2fb61c1c27a01ca5e4fd2b5c3
eradaali <erada.abdalrhman@gmail.com>
2021-06-29 15:17:50 +0300

    addingnew

--- src/App.js
+++ src/App.js
@@ -22,18 +22,21 @@ export class App extends Component {
     e.preventDefault()
     let axiosResponse = await axios.get(`https://eu1.locationiq.com/v1/search.php?key=pk.afaca60038b93085cf4229304fcb1ffc&city=${this.state.displayName}&format=json`)
 
-    this.setState({
-      displayName:axiosResponse.data[0].display_name,
-      longitude:axiosResponse.data[0].lon,
-      latitude:axiosResponse.data[0].lat
-    })
+setTimeout(()=>{
+  this.setState({
+    displayName:axiosResponse.data[0].display_name,
+    longitude:axiosResponse.data[0].lon,
+    latitude:axiosResponse.data[0].lat
+  })
+},3000)
+ 
     console.log(axiosResponse.data[0].lat)
   }
   render() {
     return (
       <div>
         <form onSubmit={this.submitData}>
-          <input type='text' placeholder='city name....' onChange={(e)=>{this.nameChangeHandler(e)}} />
+          <input type='text' placeholder='city name....' onClick={(e)=>{this.nameChangeHandler(e)}} />
           <button>Search</button>
         </form>
         <h1>{this.state.displayName}</h1>
